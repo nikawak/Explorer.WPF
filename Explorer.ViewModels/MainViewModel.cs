b24e2b904f798ce769a2931219630822c9a87100
Nikawak <nikawak228@gmail.com>
2023-07-25 20:16:32 +0300

    DelegateCommand and fix structure

--- Explorer.ViewModels/MainViewModel.cs
+++ Explorer.ViewModels/MainViewModel.cs
@@ -4,18 +4,41 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Explorer.ViewModels.Commands;
+using Explorer.ViewModels.Entities;
 
 namespace Explorer.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        public ICommand OpenCommand { get; }
+        public ObservableCollection<EntityViewModel> DirectoriesAndFiles { get; set; } = new();
+        public string CurrentPath { get; set; }
         public MainViewModel()
         {
+            OpenCommand = new DelegateCommand(Open);
             foreach(var driveName in Directory.GetLogicalDrives())
             {
                 DirectoriesAndFiles.Add(new DirectoryViewModel(driveName));
             }
         }
-        public ObservableCollection<EntityViewModel> DirectoriesAndFiles { get; set; } = new ();
+        public void Open(object param)
+        {
+            if(param is DirectoryViewModel directoryVM)
+            {
+                CurrentPath = directoryVM.FullPath;
+                DirectoriesAndFiles.Clear();
+                var dirInfo = new DirectoryInfo(CurrentPath);
+                foreach(var dir in dirInfo.GetDirectories())
+                {
+                    DirectoriesAndFiles.Add(new DirectoryViewModel(dir));
+                }
+                foreach (var file in dirInfo.GetFiles())
+                {
+                    DirectoriesAndFiles.Add(new FileViewModel(file));
+                }
+            }
+        }
     }
 }
