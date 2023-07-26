4f30693bee94a5ded40249230fc217f415a121d8
Nikawak <nikawak228@gmail.com>
2023-07-26 03:27:50 +0300

    Directory history (undo-redo)

---
+++ Explorer.ViewModels/History/DirectoryNode.cs
@@ -0,0 +1,16 @@
+﻿using Explorer.ViewModels.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.ViewModels.History
+{
+    public class DirectoryNode
+    {
+        public DirectoryNode Next { get; set; }
+        public DirectoryNode Previous { get; set; }
+        public DirectoryViewModel Current { get; set; }
+    }
+}
